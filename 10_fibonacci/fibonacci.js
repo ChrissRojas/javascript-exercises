2871dc476e9eb1321423dbfbe565642ca5f4ad2b
Christian Rojas <68656093+ChrissRojas@users.noreply.github.com>
2023-01-18 17:17:20 +0000

    Complete fib bottom up approach

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    //tabulation method PURR
+    num = parseInt(num);
+    x = 0;
+    y = 1;
+    a = x+y;
+    if (num< 0){
+        return 'OOPS';
+    }
+    else if (num==0||num==1){
+        return num;
+    }
+    for (let i = 0;i<num;i++){
+        x=y;
+        y=a;
+        a=x+y;
+    }
+    return x;
 
 };
 
