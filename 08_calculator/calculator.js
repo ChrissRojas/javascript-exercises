cbe9c849a02974e1be8898058f1ed04c21515f0d
Christian Rojas <68656093+ChrissRojas@users.noreply.github.com>
2023-01-18 16:41:28 +0000

    Complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(n1,n2) {
+	return n1+n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1,n2) {
+  return n1-n2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sumOfNums = 0
+	if(!arr.length) return 0;
+  else if(arr.length === 1) return arr[0];
+  else {
+    for(let i = 0; i<arr.length; i++){
+      sumOfNums += arr[i];
+    }
+  }
+  return sumOfNums;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let nums = args[0];
+  let product = 1;
+  for(let i =0; i<nums.length;i++){
+    product = nums[i] * product;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  // IM just going to use the built in math.pow cuz i can't be bothered.
+  return Math.pow(num, pow);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+	if (num == 0 || num == 1){
+    return 1;
+  }
+  for(let i = 1; i<=num;i++){
+    fact = fact * i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
