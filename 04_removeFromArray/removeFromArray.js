4f10e1e55c9b70524ca08a6e6f9241037bbe2914
Christian Rojas <68656093+ChrissRojas@users.noreply.github.com>
2023-01-18 15:22:00 +0000

    Complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    let arr = args[0];
+    for(let i =1; i< args.length;i++){
+        let to_remove = arr.indexOf(args[i]);
+        if (to_remove > -1 && arr[to_remove]===args[i]){
+            arr.splice(to_remove,1)
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
