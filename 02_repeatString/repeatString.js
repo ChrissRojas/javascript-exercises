c47e9c1ef2fb57231ecae14107745e2deca6dd25
Christian Rojas <68656093+ChrissRojas@users.noreply.github.com>
2023-01-18 14:08:16 +0000

    Complete repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, n) {
+    let repeat_word = '';
+    if (n<0){
+        return 'ERROR'
+    }
+    for(let i = 0; i < n; i++){
+        repeat_word += word
+    }
+    return repeat_word
 };
 
 // Do not edit below this line
