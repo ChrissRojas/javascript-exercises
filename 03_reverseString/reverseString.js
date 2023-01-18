dcc947d8bb42d37ba56bada252a765d3026cd8da
Christian Rojas <68656093+ChrissRojas@users.noreply.github.com>
2023-01-18 14:22:24 +0000

    Complete reverse with multiple solutions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,19 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    // word = word.split('')
+    // word = word.reverse()
+    // word = word.join('')
+    // return word
+    // ** Recursive version
+    // if (word === ''){
+    //     return ''
+    // }
+    // return reverseString(word.slice(1)) + word[0]
+    // ** iterative version
+    reversed = ''
+    for(let i = word.length-1; i>=0;i--){
+        reversed += word[i]
+    }
+    return reversed
 
 };
 
